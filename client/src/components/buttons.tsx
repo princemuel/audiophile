bcfe19633a8c70628d2e40d7a6ef79ba827d1085
princemuel <vansomecsam@gmail.com>
2022-09-04 04:57:44 +0100

    added button and toast components

--- client/src/components/buttons.tsx
+++ client/src/components/buttons.tsx
@@ -1,14 +1,6 @@
 import styled from 'styled-components';
 
-type Props = {};
-
-const Button = (props: Props) => {
-  return <StyledButton>Button</StyledButton>;
-};
-
-export { Button };
-
-const StyledButton = styled.button`
+export const Button = styled.button`
   display: inline-block;
   border: none;
   border-radius: var(--b-radius);
