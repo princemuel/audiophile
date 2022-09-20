37f7b3dcecdc0ec25d3dc47f993df8e723505fc3
princemuel <vansomecsam@gmail.com>
2022-09-20 20:55:44 +0100

    new commit

--- client/src/components/templates/design-system/styles.tsx
+++ client/src/components/templates/design-system/styles.tsx
@@ -1,1 +1,18 @@
-export {};
+import styled from 'styled-components';
+
+export const Heading = styled.p``;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  gap: var(--gap, 1.6rem);
+`;
+
+// .flex-col {
+//   flex-direction: column;
+// }
+
+// .flex.even-columns > * {
+//   flex-basis: 100%;
+//   outline: 1px solid palevioletred;
+// }
