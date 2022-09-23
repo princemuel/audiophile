2b7bbf23e0120f3541ea8460200ca9382d54507e
princemuel <vansomecsam@gmail.com>
2022-09-23 21:01:47 +0100

    Added Product component

--- client/src/components/pages/headphones/index.tsx
+++ client/src/components/pages/headphones/index.tsx
@@ -1,7 +1,51 @@
+import {
+  Avatar,
+  ButtonPrimary,
+  GlobalContainer,
+  Heading,
+  Text,
+} from '@src/components';
+
 type Props = {};
 
 const Headphones = (props: Props) => {
-  return <h1>Headphones</h1>;
+  return (
+    <GlobalContainer>
+      <article>
+        <figure>
+          <Avatar url={''} text={''} />
+        </figure>
+
+        <div>
+          <Heading>New product</Heading>
+          <Heading as='h2'>XX99 Mark II Headphones</Heading>
+          <Text>
+            The new XX99 Mark II headphones is the pinnacle of pristine audio.
+            It redefines your premium headphone experience by reproducing the
+            balanced depth and precision of studio-quality sound.
+          </Text>
+          <ButtonPrimary>See product</ButtonPrimary>
+        </div>
+      </article>
+
+      <article>
+        <figure>
+          <Avatar url={''} text={''} />
+        </figure>
+
+        <div>
+          <Heading>New product</Heading>
+          <Heading as='h2'>XX99 Mark II Headphones</Heading>
+          <Text>
+            The new XX99 Mark II headphones is the pinnacle of pristine audio.
+            It redefines your premium headphone experience by reproducing the
+            balanced depth and precision of studio-quality sound.
+          </Text>
+          <ButtonPrimary>See product</ButtonPrimary>
+        </div>
+      </article>
+    </GlobalContainer>
+  );
 };
 
 export { Headphones };
