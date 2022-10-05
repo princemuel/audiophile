1f5c189e21b3ac041f1eea1797cf9210237740ed
princemuel <vansomecsam@gmail.com>
2022-10-05 18:32:24 +0100

    model types

--- types/models.ts
+++ types/models.ts
@@ -1,11 +1,44 @@
-import { data } from 'common';
-import { Unarray } from './types';
-
 /*===============================*
           DATA MODELS
  *===============================*
 */
-export type IData = typeof data;
-export type IProduct = Unarray<IData>;
-export type ICategoryImage = IProduct['image'];
-export type IProductGallery = IProduct['gallery'];
+export type IData = IProduct[];
+
+export interface IProduct {
+  id: number;
+  slug: string;
+  name: string;
+  image: ResponsiveImage;
+  category: string;
+  categoryImage: ResponsiveImage;
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: ICartItem[];
+  gallery: IGallery;
+  others: Other[];
+}
+
+export interface ResponsiveImage {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export interface ICartItem {
+  quantity: number;
+  item: string;
+}
+
+export interface IGallery {
+  first: ResponsiveImage;
+  second: ResponsiveImage;
+  third: ResponsiveImage;
+}
+
+export interface Other {
+  slug: string;
+  name: string;
+  image: ResponsiveImage;
+}
