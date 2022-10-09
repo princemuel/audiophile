7929e05a3f2d1398ded8b36c495eee7e71e7cfdf
princemuel <vansomecsam@gmail.com>
2022-10-09 11:08:48 +0100

    Added Vercel url

--- lib/index.ts
+++ lib/index.ts
@@ -10,7 +10,8 @@ async function fetchProducts(url: string) {
 export function return_url() {
   return process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `${process.env?.NEXT_PUBLIC_VERCEL_URL}`;
+    : process.env?.NEXT_PUBLIC_VERCEL_URL ??
+        (process.env?.VERCEL_URL as string);
 }
 
 export async function getCategories(url: string) {
