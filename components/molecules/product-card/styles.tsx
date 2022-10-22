b2d2ae8654a57bcbc82aa8f5b81f1ddd8aa7ca49
princemuel <vansomecsam@gmail.com>
2022-10-22 17:45:01 +0100

    Cleanup product card file

--- components/molecules/product-card/styles.tsx
+++ components/molecules/product-card/styles.tsx
@@ -6,26 +6,19 @@ interface IProductListItem {
 }
 
 export const ProductListItem = styled.li<IProductListItem>`
-  --list-gap: 4rem;
-
   display: flex;
   flex-direction: column;
-  /* display: grid; */
-  gap: var(--list-gap);
+  gap: 4rem;
+  @media (min-width: 40em) {
+    gap: 5rem;
+  }
+  @media (min-width: 65em) {
+    gap: 8rem;
+    flex-direction: ${(props) => props['data-direction']};
+  }
 
   & > * {
     flex: 1;
-  }
-
-  @media (min-width: 40em) {
-    --list-gap: 5rem;
-  }
-
-  @media (min-width: 65em) {
-    --list-gap: 8rem;
-    flex-direction: ${(props) => props['data-direction']};
-
-    /* grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr)); */
   }
 `;
 
@@ -39,6 +32,7 @@ export const ProductImage = styled.figure`
 
 export const ProductBody = styled.div`
   --mx-width: 40ch;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -54,6 +48,8 @@ export const ProductBody = styled.div`
     --mx-width: 70ch;
   }
   @media (min-width: 65em) {
+    --mx-width: none;
+
     align-items: flex-start;
     justify-content: center;
     text-align: left;
@@ -61,44 +57,9 @@ export const ProductBody = styled.div`
 `;
 
 export const ProductName = styled(Heading)``;
+
 export const ProductNew = styled(Text)``;
 
 export const ProductDescription = styled(Text)`
   color: hsl(var(--clr-800) / 0.5);
 `;
-
-// export const Product = styled.article`
-//   display: flex;
-//   flex-direction: column;
-
-//   @media (min-width: 65em) {
-//     flex-direction: row;
-//     gap: var(--product-card-gap, 4em);
-
-//     &:nth-of-type(even) {
-//       flex-direction: row-reverse;
-//     }
-//   }
-
-//   & > * {
-//     flex-basis: 100%;
-//   }
-
-//   ${ProductImage} {
-//   }
-
-//   ${ProductBody} {
-//     align-self: center;
-
-//     @media (max-width: 65em) {
-//       text-align: center;
-//     }
-
-//     h2 {
-//       --flow-space: 1.6rem;
-
-//       display: flex;
-//       flex-direction: column;
-//     }
-//   }
-// `;
