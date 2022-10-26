8a50e97ff67b52e0fc8bc6a104829fe4df606cb0
princemuel <vansomecsam@gmail.com>
2022-10-26 04:20:35 +0100

    Updated back button

--- components/organisms/header/styles.tsx
+++ components/organisms/header/styles.tsx
@@ -53,21 +53,21 @@ export const HeaderNavigation = styled.nav`
 
   @media (max-width: 65em) {
     position: relative;
+    visibility: visible;
     width: 100%;
     top: 3rem;
-    /* background: var(--chakra-colors-white); */
-    padding-inline: 1.5rem;
-    padding-bottom: 2rem;
-    visibility: visible;
-    opacity: 1;
-    transform: translate(0px);
-    transition: transform 0.5s ease-in-out 0s, opacity 0.5s ease-in-out 0s;
     /* z-index: var(--chakra-zIndices-modal); */
     display: block;
     border-bottom-left-radius: 0.5rem;
     border-bottom-right-radius: 0.5rem;
     margin-inline: unset;
+    /* background: var(--chakra-colors-white); */
     background-color: hsl(0 0% 60%);
+    padding-inline: 1.5rem;
+    padding-bottom: 2rem;
+    opacity: 1;
+    translate: 0;
+    transition: translate 0.5s ease-in-out 0s, opacity 0.5s ease-in-out 0s;
   }
 `;
 
