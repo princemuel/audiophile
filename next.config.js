37c279ffe60f2bb2bfc676de7c772f04b3a3820d
Prince Muel <vansomecsam@gmail.com>
2022-11-26 20:31:12 +0100

    ci(package.json): add svgr/webpack to process svgs

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
