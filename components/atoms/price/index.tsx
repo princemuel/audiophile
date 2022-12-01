e54eae867eccce48fe2859ca8f3077b65a0b5552
Prince Muel <vansomecsam@gmail.com>
2022-12-01 06:01:54 +0100

    feat(ui): modify price component for grand total

--- components/atoms/price/index.tsx
+++ components/atoms/price/index.tsx
@@ -1,15 +1,36 @@
+import styled from 'styled-components';
+
 type Props = {
   name: string;
   price: number;
+  isGrandTotal?: boolean;
 };
 
-const PriceComponent = ({ name, price }: Props) => {
+const PriceComponent = ({ name, price, isGrandTotal }: Props) => {
   return (
-    <p>
-      <span>{name}</span>
-      <span>${price}</span>
-    </p>
+    <PriceContainer>
+      <Title className='uppercase'>{name}</Title>
+      <Price data-grand={isGrandTotal} className='fw-700 uppercase'>
+        &#36; {price}
+      </Price>
+    </PriceContainer>
   );
 };
 
 export { PriceComponent };
+
+const PriceContainer = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Title = styled.span`
+  color: hsl(var(--clr-800) / 0.5);
+`;
+
+type PriceProps = {
+  'data-grand'?: boolean;
+};
+const Price = styled.span<PriceProps>`
+  color: ${(props) => props['data-grand'] && 'var(--clr-primary-100)'};
+`;
