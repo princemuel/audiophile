cda44d330072fe6165f9bd2aa0dd50a29e1cf790
Prince Muel <vansomecsam@gmail.com>
2023-06-24 17:12:29 +0100

    feat: add project logo icon component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/logo.tsx
@@ -0,0 +1,26 @@
+import { icons } from '@/common';
+import { cn } from '@/lib';
+import Link from 'next/link';
+
+interface Props {
+  className?: string;
+}
+
+const LogoIcon = ({ className }: Props) => {
+  return (
+    <Link
+      href={'/'}
+      aria-label='Go to Home'
+      title='Home'
+      className={cn(className)}
+    >
+      <span className='sr-only'>Go to Home</span>
+      <icons.site.logo
+        aria-hidden='true'
+        className='fill-current stroke-transparent'
+      />
+    </Link>
+  );
+};
+
+export { LogoIcon };
