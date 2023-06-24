f27defde623ca71f62bb06e5cfd09573d6d7bf3a
Prince Muel <vansomecsam@gmail.com>
2023-06-24 17:17:02 +0100

    feat: scaffold button component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/button-or-link.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+type ButtonOrLinkProps = React.ComponentProps<'button'> &
+  React.ComponentProps<'a'>;
+
+interface ButtonProps extends ButtonOrLinkProps {
+  children: React.ReactNode;
+  href?: __next_route_internal_types__.RouteImpl<string>;
+}
+
+/**
+ * This is a base component that will render either a button or a link,
+ * depending on the props that are passed to it. The link rendered will
+ * also correctly get wrapped in a next/link component to ensure ideal
+ * page-to-page transitions.
+ */
+export function ButtonOrLink({ href, ...props }: ButtonProps) {
+  const isAnchor = typeof href !== 'undefined';
+  const ButtonOrLink = isAnchor ? 'a' : 'button';
+
+  const element = <ButtonOrLink {...props} />;
+
+  if (isAnchor) {
+    return (
+      <Link href={href} legacyBehavior={true}>
+        {element}
+      </Link>
+    );
+  }
+
+  return element;
+}
