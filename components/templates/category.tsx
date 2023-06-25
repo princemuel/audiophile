008ed95c94476217dbe4184eeed248678b2a40b7
Prince Muel <vansomecsam@gmail.com>
2023-06-25 15:31:30 +0100

    feat: scaffold category page template
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/templates/category.tsx
+++ components/templates/category.tsx
@@ -1,7 +1,24 @@
-interface Props {}
+import { cn } from '@/lib';
+import { ProductCard } from '../molecules';
 
-const CategoryTemplate = (props: Props) => {
-  return <div>CategoryTemplate</div>;
+interface Props {
+  products: IProduct[];
+}
+
+const CategoryTemplate = ({ products }: Props) => {
+  return (
+    <>
+      <section className={cn('h-container')}>
+        {products.map((product, idx) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            priority={idx === 0}
+          />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export { CategoryTemplate };
