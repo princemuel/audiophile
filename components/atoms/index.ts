ff07a25ac838337bdb3b64462b661ab178bde184
Prince Muel <vansomecsam@gmail.com>
2023-06-25 15:36:38 +0100

    fix: resolve build issue
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -3,6 +3,5 @@ export * from './button-or-link';
 export * from './logo';
 export * from './navlink';
 export * from './responsive-image';
-export * from './shimmer.local';
 export * from './social-icon';
 export * from './text';
