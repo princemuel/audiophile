f3848dd2bb487bb3d13f29e67a965414cf041a4d
Prince Muel <vansomecsam@gmail.com>
2023-06-25 14:14:08 +0100

    feat: scaffold data retrieval on pages
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,7 +1,60 @@
-export default function PageRoute() {
+import { notFound } from 'next/navigation';
+import { getAllProductCategories } from '../database/get-all-categories';
+import { getProductsByCategory } from '../database/get-by-category';
+
+interface Props {
+  params: IParams;
+}
+
+const PageRoute = async ({ params: { category } }: Props) => {
+  const products = await getProductsByCategory(`${category}`);
+  if (!products) notFound();
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Page</h1>
+      <div>{JSON.stringify(products, null, 2)}</div>
     </main>
   );
+};
+
+export default PageRoute;
+
+export async function generateStaticParams() {
+  const categories = await getAllProductCategories();
+  return (categories || []).map((category) => {
+    return { category };
+  });
 }
+
+// export async function generateMetadata({
+//   params: { category },
+// }: Props): Promise<Metadata> {
+//   const product = await getProductsByCategory(`${category}`);
+
+//   if (!product) {
+//     return {
+//       title: 'Project Not Found',
+//       description: 'The requested resource does not exist',
+//     };
+//   }
+
+//   return {
+//     title: project.meta.title,
+//     description: project.meta.description,
+//     keywords: project.meta.tags,
+//     openGraph: {
+//       type: 'article',
+//       title: project.meta.title,
+//       description: project.meta.description,
+//       authors: ['Prince Muel'],
+//       publishedTime: new Date(project.meta.date).toISOString(),
+//       images: project.meta.links.thumbnail,
+//     },
+//     twitter: {
+//       title: project.meta.title,
+//       description: project.meta.description,
+//       images: project.meta.links.thumbnail,
+//     },
+//   } satisfies Metadata;
+// }
