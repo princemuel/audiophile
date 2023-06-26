6d09b45c63e92d26c6694e0196f5f93b5f54ce88
Prince Muel <vansomecsam@gmail.com>
2023-06-26 15:45:38 +0100

    perf: update getbyparams function
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/database/get-by-params.ts
+++ app/database/get-by-params.ts
@@ -2,14 +2,12 @@ import { cache } from 'react';
 import 'server-only';
 import { fetchAllProducts } from './fetch-all-products';
 
-export const preload = (slug: string, category: string) => {
-  void getProductByParams(slug, category);
+export const preload = ({ slug, category }: IParams) => {
+  void getProductByParams({ slug, category });
 };
-export const getProductByParams = cache(
-  async (slug: string, category: string) => {
-    const products = await fetchAllProducts();
-    return products?.find(
-      (product) => product.slug === slug && product?.category === category
-    );
-  }
-);
+export const getProductByParams = cache(async ({ slug, category }: IParams) => {
+  const products = await fetchAllProducts();
+  return products?.find(
+    (product) => product.slug === slug && product?.category === category
+  );
+});
