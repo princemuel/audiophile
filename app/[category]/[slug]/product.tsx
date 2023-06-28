f50523049578618c4a1bf5de7199ef91d6a54622
Prince Muel <vansomecsam@gmail.com>
2023-06-28 21:15:51 +0100

    feat: update product details template
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/[category]/[slug]/product.tsx
+++ app/[category]/[slug]/product.tsx
@@ -1,8 +1,9 @@
+import featuresStyles from '@/assets/styles/features.module.scss';
+import galleryStyles from '@/assets/styles/gallery.module.scss';
 import { ImageBestGearJPG, icons, links } from '@/common';
-import { Button, ProductCardB, ResponsiveImage, Text } from '@/components';
+import { Button, ProductCard, ResponsiveImage, Text } from '@/components';
 import { capitalize, cn } from '@/lib';
 import Link from 'next/link';
-
 interface Props {
   product: IProduct;
 }
@@ -15,13 +16,20 @@ const ProductDetailsTemplate = ({ product }: Props) => {
       </section>
 
       <section className={cn('h-container')}>
-        <ProductCardB product={product} priority={true} />
+        <ProductCard product={product} priority={true} cart={true} />
       </section>
 
       <section className={cn('h-container')}>
-        <article>
-          <header>
-            <Text as='h2'>Features</Text>
+        <article
+          className={cn(
+            'flex items-start justify-between gap-20',
+            featuresStyles.features
+          )}
+        >
+          <header className='flex flex-col items-start gap-12'>
+            <Text as='h2' variant={'primary'} size={'xl'} weight={'bold'}>
+              Features
+            </Text>
 
             {product?.features?.split('\n\n')?.map((paragraph) => (
               <Text as='p' key={paragraph?.charAt(1)}>
@@ -30,14 +38,20 @@ const ProductDetailsTemplate = ({ product }: Props) => {
             ))}
           </header>
 
-          <div>
-            <Text as='h3'>In the box</Text>
+          <div className='flex basis-full flex-col gap-12'>
+            <Text as='h3' variant={'primary'} size={'xl'} weight={'bold'}>
+              In the box
+            </Text>
 
-            <ul>
+            <ul className='flex flex-col gap-4'>
               {product?.includes?.map((included) => (
-                <li key={included?.item}>
-                  <Text as='strong'>{included?.quantity}x</Text>
-                  <Text as='span'>{included?.item}</Text>
+                <li key={included?.item} className='flex items-center gap-6'>
+                  <Text as='strong' variant={'secondary'} weight={'bold'}>
+                    {included?.quantity}x
+                  </Text>
+                  <Text as='span' variant={'primary/50'}>
+                    {included?.item}
+                  </Text>
                 </li>
               ))}
             </ul>
@@ -46,19 +60,26 @@ const ProductDetailsTemplate = ({ product }: Props) => {
       </section>
 
       <section className={cn('h-container')}>
-        <div>
+        <div className={cn('', galleryStyles.gallery)}>
           {Object.entries(product?.gallery).map(([key, value]) => (
             <ResponsiveImage
               key={key}
               src={value?.desktop}
               alt={`A picture of ${product?.name}`}
+              className=''
+              container='rounded-brand overflow-clip'
             />
           ))}
         </div>
       </section>
 
-      <section className={cn('h-container')}>
-        <ul>
+      <section className={cn('flex flex-col gap-24 h-container')}>
+        <header className='flex items-center justify-center'>
+          <Text as='h2' variant={'primary'} size={'xl'} weight={'bold'}>
+            You may also like
+          </Text>
+        </header>
+        <ul className='flex flex-col items-center gap-12 md:flex-row'>
           {product?.others?.map((other) => {
             // SHAME FUNCTION: CHEATED 😂
             let category = other?.slug.split('-').at(-1);
@@ -67,13 +88,16 @@ const ProductDetailsTemplate = ({ product }: Props) => {
                 ? category
                 : category + 's';
             return (
-              <li key={other?.name}>
+              <li key={other?.name} className='flex flex-1 flex-col gap-12'>
                 <ResponsiveImage
                   src={other?.image?.desktop}
                   alt={`A picture of ${other?.name}`}
+                  container='rounded-brand overflow-clip'
                 />
-                <div>
-                  <Text as='h4'>{other?.name}</Text>
+                <div className='flex flex-col items-center gap-12'>
+                  <Text as='h4' variant={'primary'} size={'md'} weight={'bold'}>
+                    {other?.name}
+                  </Text>
                   <Button href={`/${category}/${other?.slug}`}>
                     See Product
                   </Button>
