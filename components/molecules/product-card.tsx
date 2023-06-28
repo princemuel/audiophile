adb51cd0a37564631b448f7d75ec0a07c713cc56
Prince Muel <vansomecsam@gmail.com>
2023-06-28 21:13:39 +0100

    feat: refactor product card component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/product-card.tsx
+++ components/molecules/product-card.tsx
@@ -1,36 +1,97 @@
-import { cn } from '@/lib';
+import { cn, formatPrice } from '@/lib';
 import { Button, ResponsiveImage, Text } from '../atoms';
 
 interface Props {
   product: IProduct;
   priority: boolean;
+  cart?: boolean;
 }
 
-const ProductCard = ({ product, priority }: Props) => {
+const ProductCard = ({ product, priority, cart }: Props) => {
   return (
-    <article className={cn('')}>
+    <article className={cn('flex items-center gap-20')}>
       <ResponsiveImage
         src={product.categoryImage.desktop}
         alt={product.name}
         priority={priority}
         sizes='100vw'
         style={{ width: 'auto' }}
+        container='flex-1 rounded-brand overflow-clip'
       />
 
-      <div className={cn('')}>
-        {product?.new && <Text>New Product</Text>}
-        <Text as='h2' variant={'primary'}>
+      <div className={cn('flex flex-1 flex-col gap-12')}>
+        {product?.new && (
+          <Text
+            as='h4'
+            variant={'secondary'}
+            size={'sx'}
+            uppercase
+            aria-live='polite'
+          >
+            New Product
+          </Text>
+        )}
+
+        <Text
+          as='h1'
+          variant={'primary'}
+          size={'2xl'}
+          weight={'bold'}
+          aria-live='polite'
+        >
           {product?.name}
         </Text>
 
-        <Text as='p' variant={'primary'}>
+        <Text as='p' variant={'primary/50'}>
           {product?.description}
         </Text>
 
-        {/* @ts-expect-error  */}
-        <Button href={`${product.category}/${product?.slug}`}>
-          See Product
-        </Button>
+        {cart && (
+          <Text as='p' variant={'primary'}>
+            {formatPrice(product?.price)}
+          </Text>
+        )}
+
+        <div className='flex items-center gap-6'>
+          {cart ? (
+            <>
+              <div className='flex items-center gap-4 bg-zinc-50'>
+                <Button type='button' variant={'counter'} size={'sm'}>
+                  <span>&#x2212;</span>
+                </Button>
+                <Text
+                  as='output'
+                  variant={'primary'}
+                  size={'xs'}
+                  weight={'bold'}
+                >
+                  1
+                </Text>
+                <Button type='button' variant={'counter'} size={'sm'}>
+                  &#43;
+                </Button>
+              </div>
+              <Button type='button' uppercase={true}>
+                Add to cart
+              </Button>
+            </>
+          ) : (
+            <Button
+              // @ts-expect-error
+              href={`${product.category}/${product?.slug}`}
+              uppercase={true}
+            >
+              See Product
+            </Button>
+          )}
+          {/* <Button type='button' variant={'neutral'}>
+            Add to cart
+          </Button>
+          <Button type='button' variant={'chevron'} size={'none'}>
+            <span>Shop</span>
+            <icons.chevron.right />
+          </Button> */}
+        </div>
       </div>
     </article>
   );
