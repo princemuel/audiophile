ecb280e88f74dfcb1877a6c25cfeba75f10b5282
Prince Muel <vansomecsam@gmail.com>
2023-07-05 16:07:32 +0100

    perf: refactor/improve button component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- @types/globals.d.ts
+++ @types/globals.d.ts
@@ -1,8 +1,3 @@
-export declare module 'react' {
-  function forwardRef<T, P = {}>(
-    render: (props: P, ref: React.Ref<T>) => React.ReactElement | null
-  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
-}
 // Allow TypeScript to import SVG files using Webpack's `svgr` loader.
 declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
