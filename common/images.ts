0e636388a94ac0fd498946def887b69552feb2c9
Prince Muel <vansomecsam@gmail.com>
2023-07-05 21:17:12 +0100

    feat: add more svgs to assets dir
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- common/images.ts
+++ common/images.ts
@@ -1,10 +1,12 @@
 import IconArrowRightSVG from '@/assets/svgs/icon-arrow-right.inline.svg';
 import IconCartSVG from '@/assets/svgs/icon-cart.inline.svg';
+import IconCashOnDelivery from '@/assets/svgs/icon-cash-on-delivery.inline.svg';
 import IconFacebookSVG from '@/assets/svgs/icon-facebook.inline.svg';
 import IconGithubSVG from '@/assets/svgs/icon-github.inline.svg';
 import IconHamburgerSVG from '@/assets/svgs/icon-hamburger.inline.svg';
 import IconInstagramSVG from '@/assets/svgs/icon-instagram.inline.svg';
 import IconLinkedInSVG from '@/assets/svgs/icon-linkedin.inline.svg';
+import IconOrderConfirm from '@/assets/svgs/icon-order-confirmation.inline.svg';
 import IconTwitterSVG from '@/assets/svgs/icon-twitter.inline.svg';
 import IconLogoSVG from '@/assets/svgs/logo.inline.svg';
 import ImageBestGearJPG from '@/public/assets/shared/desktop/image-best-gear.jpg';
@@ -15,12 +17,14 @@ import ImageThumbnailSpeakerPNG from '@/public/assets/shared/desktop/image-categ
 export {
   IconArrowRightSVG,
   IconCartSVG,
+  IconCashOnDelivery,
   IconFacebookSVG,
   IconGithubSVG,
   IconHamburgerSVG,
   IconInstagramSVG,
   IconLinkedInSVG,
   IconLogoSVG,
+  IconOrderConfirm,
   IconTwitterSVG,
   ImageBestGearJPG,
   ImageThumbnailEarphonePNG,
