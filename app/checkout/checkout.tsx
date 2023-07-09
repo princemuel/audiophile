19f06d779ebeb0d98b52dcdc74c0634d4a5bdb52
Prince Muel <vansomecsam@gmail.com>
2023-07-09 16:16:00 +0100

    chore: cleanup checkout page
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/checkout/checkout.tsx
+++ app/checkout/checkout.tsx
@@ -1,15 +1,8 @@
-'use client';
-
 import { BackButton, CheckoutForm } from '@/components';
-
-// import { useZodForm } from "@/lib";
 
 interface Props {}
 
 const CheckoutTemplate = (props: Props) => {
-  // const methods  = useZodForm({
-  //   schema: null
-  // })
   return (
     <div className='h-container'>
       <div className='my-28'>
