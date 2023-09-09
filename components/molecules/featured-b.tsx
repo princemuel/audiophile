a68a651234ab17bcf0598d61cffe2fc781853d33
Prince Muel <vansomecsam@gmail.com>
2023-09-10 00:52:56 +0100

    feat: update featured products styles
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/featured-b.tsx
+++ components/molecules/featured-b.tsx
@@ -1,18 +1,18 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { Button, Text } from '../atoms';
 
 const FeaturedTwo = () => {
   return (
     <article
-      className={`rounded-brand bg-[url('/assets/home/mobile/image-speaker-zx7.jpg')] bg-cover bg-[center_right] bg-no-repeat px-20 py-60 sm:bg-[url('/assets/home/tablet/image-speaker-zx7.jpg')] md:px-32 md:py-40 lg:bg-[url('/assets/home/desktop/image-speaker-zx7.jpg')]`}
+      className={`rounded-brand bg-[url('/assets/home/mobile/image-speaker-zx7.jpg')] bg-cover bg-[center_right] bg-no-repeat px-10 py-36 sm:bg-[url('/assets/home/tablet/image-speaker-zx7.jpg')] md:px-20 md:py-24 lg:bg-[url('/assets/home/desktop/image-speaker-zx7.jpg')]`}
     >
       <div className='flex flex-col items-start gap-12'>
-        <Text as='h3' size={'lg'} weight={'bold'}>
+        <Text as='h3' variant={'monochrome'} size={'large'} weight={'bold'}>
           ZX7 Speaker
         </Text>
 
-        <Button variant={'neutral'} asChild>
-          <Link href={`/speakers/zx7-speaker`}>See Product</Link>
+        <Button type='button' variant={'monochrome'} size={'medium'} asChild>
+          <NextLink href={`/speakers/zx7-speaker`}>See Product</NextLink>
         </Button>
       </div>
     </article>
