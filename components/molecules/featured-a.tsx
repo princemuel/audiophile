7e2516b073e3004dca88b7b69d5d952ff28a7dc3
Prince Muel <vansomecsam@gmail.com>
2023-09-10 01:48:00 +0100

    feat: update featured product styles
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/featured-a.tsx
+++ components/molecules/featured-a.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button, Text } from '../atoms';
+import NextLink from 'next/link';
+import { Button, NextImage, Text } from '../atoms';
+// feat: update featured products styles
 
 const FeaturedOne = () => {
   return (
     <article
-      className={`grid grid-cols-1 gap-12 overflow-y-hidden rounded-brand bg-brand-500 bg-[url('/assets/home/desktop/pattern-circles.svg')] bg-cover bg-[center_-14rem] bg-no-repeat px-12 py-20 md:grid-cols-2 md:gap-40  md:bg-[-25rem_-5rem] md:px-40 md:pb-0 md:pt-40`}
+      className={`grid grid-cols-1 gap-7 overflow-y-hidden rounded-brand bg-brand-500 bg-[url('/assets/home/desktop/pattern-circles.svg')] bg-cover bg-[center_-14rem] bg-no-repeat px-7 py-12 md:grid-cols-2 md:gap-24  md:bg-[-16rem_-3rem] md:px-24 md:pb-0 md:pt-24`}
     >
       <figure className='max-w-xs place-self-center md:max-w-none md:translate-y-6'>
-        <Image
+        <NextImage
           src={'/assets/home/desktop/image-speaker-zx9.png'}
           width='540'
           height='680'
@@ -21,27 +21,21 @@ const FeaturedOne = () => {
       <div className='flex flex-col items-center gap-12 text-center md:my-auto md:items-start md:text-left'>
         <Text
           as='h3'
-          variant={'inverted'}
-          weight={'bold'}
-          uppercase
-          className='text-[clamp(4rem,5vw,5.6rem)]'
+          modifier='inverted'
+          size={'3xl'}
+          weight='bold'
+          className='w-[min-content] whitespace-break-spaces text-[clamp(2.5rem,5vw,3.5rem)]'
         >
-          <span>ZX9</span>
-          <br />
-          <span>Speaker</span>
+          ZX9 Speaker
         </Text>
 
-        <Text as='p' variant={'inverted'} className=' max-w-[35ch]'>
+        <Text as='p' modifier='inverted' className='max-w-[35ch]'>
           Upgrade to premium speakers that are phenomenally built to deliver
           truly remarkable sound.
         </Text>
 
-        <Button
-          variant={'none'}
-          className='bg-black text-white hover:bg-neutral-700 focus:bg-neutral-700'
-          asChild
-        >
-          <Link href={`/speakers/zx9-speaker`}>See Product</Link>
+        <Button type='button' variant={'monochrome'} size={'medium'} asChild>
+          <NextLink href={`/speakers/zx9-speaker`}>See Product</NextLink>
         </Button>
       </div>
     </article>
