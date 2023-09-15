e1d7bd1e1f42f2d16438d5a7e86d58861567a7c2
Prince Muel <vansomecsam@gmail.com>
2023-09-15 23:45:28 +0100

    feat: update helper functions
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- helpers/storage.ts
+++ helpers/storage.ts
@@ -3,20 +3,22 @@ export function removeFromStorage(key: string) {
 }
 
 class StorageSingleton {
-  private static instance: StorageSingleton;
+  private static instance: Readonly<StorageSingleton>;
 
   private constructor() {}
 
-  public static getInstance(): StorageSingleton {
+  public static getInstance(): Readonly<StorageSingleton> {
     if (!StorageSingleton.instance) {
-      StorageSingleton.instance = new StorageSingleton();
+      StorageSingleton.instance = Object.freeze(new StorageSingleton());
     }
 
     return StorageSingleton.instance;
   }
+
   clear() {
     localStorage.clear();
   }
+
   removeItem(key: string) {
     localStorage.removeItem(key);
   }
@@ -40,8 +42,8 @@ class StorageSingleton {
     }
   }
 
-  storageAvailable(type: 'localStorage' | 'sessionStorage') {
-    let storage;
+  isAvailable(type: 'localStorage' | 'sessionStorage') {
+    let storage: Storage | null = null;
     try {
       storage = window[type];
       const x = '__storage_test__';
@@ -69,3 +71,24 @@ class StorageSingleton {
 }
 
 export const storage = StorageSingleton.getInstance();
+
+// interface ObserverType<T> {
+//   observers: Set<(data?: T) => void>;
+//   notify(data?: T): void;
+//   subscribe(callback: (data?: T) => void): () => void;
+// }
+
+// const Observable = Object.freeze({
+//   observers: new Set(),
+//   notify(data) {
+//     this?.observers?.forEach((observer) => data ? observer(data) : observer());
+//   },
+//   subscribe(callback: () => void) {
+//     this.observers.add(callback);
+//     return () => {
+//       this.observers.delete(callback);
+//     };
+//   },
+// });
+
+// Observable.notify();
