0e262fdfcc35b7abf4a085f6395d4fc370ac9b05
Prince Muel <vansomecsam@gmail.com>
2023-09-28 01:28:28 +0100

    feat: rename components
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/featured-b.tsx
+++ components/molecules/featured-b.tsx
@@ -1,7 +1,7 @@
 import NextLink from 'next/link';
 import { Button, Text } from '../atoms';
 
-const FeaturedTwo = () => {
+const SecondFeaturedProduct = () => {
   return (
     <article
       className={`rounded-lg bg-[url('/assets/home/mobile/image-speaker-zx7.jpg')] bg-cover bg-[center_right] bg-no-repeat px-10 py-36 sm:bg-[url('/assets/home/tablet/image-speaker-zx7.jpg')] md:px-20 md:py-24 lg:bg-[url('/assets/home/desktop/image-speaker-zx7.jpg')]`}
@@ -25,4 +25,4 @@ const FeaturedTwo = () => {
   );
 };
 
-export { FeaturedTwo };
+export { SecondFeaturedProduct };
