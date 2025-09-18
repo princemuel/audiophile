937711ef4946214fe13a9e4cc7b731c91759d801
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-09-18 16:19:08 +0100

    chore(deps): bump packages to latest

--- @types/project.d.ts
+++ @types/project.d.ts
@@ -1,15 +1,1 @@
-import type { VariantProps } from 'cva';
-
-export type RequiredVariantProps<
-  T extends (...args: any) => any,
-  // By default, all variants will be required
-  Keys extends keyof VariantProps<T> = keyof VariantProps<T>,
-> = Simplify<
-  // Create an intersection of all variants with those being marked as required
-  VariantProps<T> & {
-    // For each variant being marked as required, remove null and undefined
-    [Variant in Keys]: Exclude<VariantProps<T>[Variant], null | undefined>;
-  }
->;
-
 type Simplify<T> = { [K in keyof T]: T[K] } & NonNullable<unknown>;
---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next';
+
+const config: NextConfig = {
+  reactStrictMode: true,
+  typedRoutes: true,
+};
+
+export default config;
